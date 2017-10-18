a16c1a83daf1b6c07eed4657c1b08b6461110e3f
chicklin <493748267@qq.com>
2017-10-18 22:29:36 +0800

    express reply over

--- ky.php
+++ ky.php
@@ -23,7 +23,8 @@ class wechatCallbackapiTest
         $echoStr = $_GET["echostr"];
 
         //valid signature , option
-        if($this->checkSignature()){
+        if($this->checkSignature())
+        {
             echo $echoStr;
             exit;
         }
@@ -46,9 +47,12 @@ class wechatCallbackapiTest
         $tmpStr = sha1($tmpStr);
         
         //开发者获得加密后的字符串与signature对比
-        if($tmpStr == $signature){
+        if($tmpStr == $signature)
+        {
             return true;
-        }else{
+        }
+        else
+        {
             return false;
         }
     }
@@ -68,7 +72,8 @@ class wechatCallbackapiTest
     	$msgType = $xmlObj->MsgType;//消息的类型
 
     	//根据消息类型进行业务处理
-    	switch ($msgType) {
+    	switch ($msgType) 
+        {
     		case 'event':
     			echo $this->receiveEvent($xmlObj);
     			break;
@@ -95,10 +100,12 @@ class wechatCallbackapiTest
     {
     	//$content = trim($obj->Content);//获取文本消息的内容
         $content = str_replace(" ","",$obj->Content);
+        $content = str_replace("+","",$content);
         $keyword = mb_substr($content,0,2,'utf-8');//截取回复消息的前两个中文
-    	switch ($keyword) {
+    	switch ($keyword) 
+        {
     		case '功能':
-    			$replyStr = "查询功能介绍(回复关键字)：\n1、志愿时\n2、天气+城市名3、4、5";
+    			$replyStr = "查询功能介绍(回复关键字)：\n1、志愿时\n2、天气+城市名\n3、快递+单号\n4、\n5";
     			return $this->replyText($obj,$replyStr);
     			break;
 
@@ -110,8 +117,32 @@ class wechatCallbackapiTest
             case '天气':
                 $cityname = mb_substr($content,2,6,'utf-8');//获得城市名
                 $weather_json = $this->getWeather($cityname);
-                $replyStr = $this->replyWeather($weather_json);
-                return $this->replyText($obj,$replyStr);
+                if (json_decode($weather_json)->{'status'} == 0) 
+                {
+                    $replyStr = $this->replyWeather($weather_json);
+                    return $this->replyText($obj,$replyStr);
+                }
+                else
+                {
+                    $replyStr = "输入数据有误，请重新输入";
+                    return $this->replyText($obj,$replyStr);
+                }
+                break;
+
+            case '快递':
+                $code = mb_substr($content,2,NULL,'utf-8');
+                $express_json = $this->getExpress($code);
+                if (json_decode($express_json)->{'status'} == 0) 
+                {
+                    $replyStr = $this->replyExpress($express_json);
+                    return $this->replyText($obj,$replyStr);
+                }
+                else
+                {
+                    $replyStr = "输入数据有误，请重新输入";
+                    return $this->replyText($obj,$replyStr);
+                }
+
                 break;
     		
     		default:
@@ -135,6 +166,7 @@ class wechatCallbackapiTest
 		return sprintf($replyTextMsg, $obj->FromUserName, $obj->ToUserName, time(), $contentStr);
     }
 
+    //接受图片消息
     public function receiveImage($obj)
     {
     	$picUrl = $obj->PicUrl;//获取图片的URL
@@ -142,7 +174,7 @@ class wechatCallbackapiTest
     	$picArr = array('picUrl'=>$picUrl,'mediaId'=>$mediaId);
     	return $this->replyImage($obj,$picArr);
     }
-
+    //回复图片消息
     public function replyImage($obj,$array)
     {
     	//回复图片消息
@@ -157,10 +189,11 @@ class wechatCallbackapiTest
 		                </xml>";
 		return sprintf($replyImageMsg,$obj->FromUserName,$obj->ToUserName,time(),$array['mediaId']);
     }
-
+    //接受事件消息
     public function receiveEvent($obj)
     {
-		switch ($obj->Event) {
+		switch ($obj->Event) 
+        {
 			case 'subscribe':
 				$replyContent = "欢迎关注！回复“功能”看看我能干什么吧！";
 				return $this->replyText($obj,$replyContent);
@@ -171,7 +204,7 @@ class wechatCallbackapiTest
 				break;
 		}
     }
-
+    //获取天气json
     public function getWeather($cityname)
     {
         $host = "http://jisutianqi.market.alicloudapi.com";
@@ -198,10 +231,10 @@ class wechatCallbackapiTest
         }
         //var_dump(curl_exec($curl));
         $result = curl_exec($curl);
-        //curl_close($curl);
+        curl_close($curl);
         return $result;
     }
-
+    //回复天气消息
     public function replyWeather($weather_json)
     {
         $arr = json_decode($weather_json);
@@ -209,6 +242,48 @@ class wechatCallbackapiTest
         $replyText = "城市: ".$result->{'city'}."\n更新时间: ".$result->{'updatetime'}."\n----------\n"."1、天气速览\n天气: ".$result->{'weather'}."\n温度: ".$result->{'temp'}."°C (最高温: ".$result->{'temphigh'}."°C 最低温: ".$result->{'templow'}."°C)\n湿度: ".$result->{'humidity'}."%\n风力: ".$result->{'winddirect'}.$result->{'windpower'}."\n空气: ".$result->{'aqi'}->{'quality'}."\n".$result->{'index'}[2]->{'iname'}.": ".$result->{'index'}[2]->{'ivalue'}."\n----------\n"."2、空气质量\nPM2.5: ".$result->{'aqi'}->{'pm2_5'}."\nPM10: ".$result->{'aqi'}->{'pm10'}."\nSO2: ".$result->{'aqi'}->{'so2'}."\nNO2: ".$result->{'aqi'}->{'no2'}."\nCO: ".$result->{'aqi'}->{'co'}."\n指数: ".$result->{'aqi'}->{'aqiinfo'}->{'level'}."\n影响: ".$result->{'aqi'}->{'aqiinfo'}->{'affect'}."\n----------\n"."3、未来6小时天气: \n".$result->{'hourly'}[0]->{'time'}.' '.$result->{'hourly'}[0]->{'weather'}.' '.$result->{'hourly'}[0]->{'temp'}."°C\n".$result->{'hourly'}[1]->{'time'}.' '.$result->{'hourly'}[1]->{'weather'}.' '.$result->{'hourly'}[1]->{'temp'}."°C\n".$result->{'hourly'}[2]->{'time'}.' '.$result->{'hourly'}[2]->{'weather'}.' '.$result->{'hourly'}[2]->{'temp'}."°C\n".$result->{'hourly'}[3]->{'time'}.' '.$result->{'hourly'}[3]->{'weather'}.' '.$result->{'hourly'}[3]->{'temp'}."°C\n".$result->{'hourly'}[4]->{'time'}.' '.$result->{'hourly'}[4]->{'weather'}.' '.$result->{'hourly'}[4]->{'temp'}."°C\n".$result->{'hourly'}[5]->{'time'}.' '.$result->{'hourly'}[5]->{'weather'}.' '.$result->{'hourly'}[5]->{'temp'}."°C"."\n----------\n"."3、未来3天天气: \n".$result->{'daily'}[1]->{'date'}.' '.$result->{'daily'}[1]->{'week'}.' '.$result->{'daily'}[1]->{'day'}->{'weather'}.' '.$result->{'daily'}[1]->{'night'}->{'templow'}.'-'.$result->{'daily'}[1]->{'day'}->{'temphigh'}."°C\n".$result->{'daily'}[2]->{'date'}.' '.$result->{'daily'}[2]->{'week'}.' '.$result->{'daily'}[2]->{'day'}->{'weather'}.' '.$result->{'daily'}[2]->{'night'}->{'templow'}.'-'.$result->{'daily'}[2]->{'day'}->{'temphigh'}."°C\n".$result->{'daily'}[3]->{'date'}.' '.$result->{'daily'}[3]->{'week'}.' '.$result->{'daily'}[3]->{'day'}->{'weather'}.' '.$result->{'daily'}[3]->{'night'}->{'templow'}.'-'.$result->{'daily'}[3]->{'day'}->{'temphigh'}."°C\n";
         return $replyText;
     }
+    //获取快递json
+    public function getExpress($code)
+    {
+        $host = "http://jisukdcx.market.alicloudapi.com";
+        $path = "/express/query";
+        $method = "GET";
+        $appcode = "7830cf6348c444d1a1455e70fb5f434e";
+        $headers = array();
+        array_push($headers, "Authorization:APPCODE " . $appcode);
+        $querys = "number={$code}&type=auto";
+        $bodys = "";
+        $url = $host . $path . "?" . $querys;
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_FAILONERROR, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        if (1 == strpos("$".$host, "https://"))
+        {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        }
+        $result = curl_exec($curl);
+        curl_close($curl);
+        return $result;
+    }
+    //回复快递消息
+    public function replyExpress($express_json)
+    {
+        $result = json_decode($express_json);
+        $arr = $result->{"result"}->{'list'};
+        $replyText = '';
+        for ($i=0; $i < count($arr); $i++) 
+        { 
+            $replyText .= $arr[$i]->{'time'}." ".$arr[$i]->{'status'}."\n";
+        }
+        return $replyText;
+    }
+
 }
 
 ?>
